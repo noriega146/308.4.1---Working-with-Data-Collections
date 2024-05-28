4436999af7efcac3204db349fe77344ea8a6d3f9
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-05-28 14:30:06 -0400

    Create arrays.js
    // Part 1: Refactoring Old Code

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const csvString = "ID,Name,Occupation,Age\r\n42,Bruce,Knight,41\r\n57,Bob,Fry Cook,19\r\n63,Blaine,Quiz Master,58\r\n98,Bill,Doctor’s Assistant,26";
+// Part 1: Refactoring Old Code
+// Split the CSV string into rows
+//let rows = csvString.split('\r\n');
+
+// Parse each row into an array of cells
+//let data = rows.map(row => row.split(','));
+
+//console.log(data);
+
+//Part 2: Expanding Functionality
+
